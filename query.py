a21050c2ea6246bee36cd88c659d923aec8f1cb9
rimazk123 <rimaz_khan@yahoo.com>
2019-10-13 19:29:25 -0400

    Removed keys

--- query.py
+++ query.py
@@ -5,8 +5,8 @@ import json
 import random
 
 credentials = oauth2.SpotifyClientCredentials(
-        client_id='15e7265b21d14a6eadbff697e2d4fa6b',
-        client_secret='f6d462e641f1424f973eb80e01c9d46b')
+        client_id='YOUR_ID_HERE',
+        client_secret='YOUR_SECRET_HERE')
 
 token = credentials.get_access_token()
 spotify = spotipy.Spotify(auth=token)
